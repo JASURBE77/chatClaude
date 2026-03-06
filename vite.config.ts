acdc29f75caafad0324ee5c198e59097c0c082bc
JASURBEE <animesila89@gmail.com>
2026-03-06 20:34:05 +0500

    WDefv

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,21 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(),  tailwindcss(),],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router-dom'],
+          antd: ['antd', '@ant-design/icons'],
+          emoji: ['emoji-mart', '@emoji-mart/data', '@emoji-mart/react'],
+          socket: ['socket.io-client'],
+        },
+      },
+    },
   },
 })
